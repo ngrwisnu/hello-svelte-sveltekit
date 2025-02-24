992a645b4395656057be97f679cecdf51f39f3cc
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-02-24 10:08:23 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	const clickHandler = () => alert('clicked');
+</script>
+
+<h1 class="text-2xl font-semibold">Welcome to SvelteKit</h1>
+<button onclick={clickHandler}>click me</button>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
