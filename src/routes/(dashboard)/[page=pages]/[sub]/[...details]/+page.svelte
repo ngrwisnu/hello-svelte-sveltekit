2d7fc5c73a4bab8b8734e29d9e9fdab51c2fffbf
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-03-10 13:47:02 +0700

    add page for product details

---
+++ src/routes/(dashboard)/[page=pages]/[sub]/[...details]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import ProductDetails from '$lib/pages/ProductDetails.svelte';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+
+	let detailId = $derived(data.details.split('/')[0]);
+	let Pages = $derived.by(() => {
+		const routesMap: {
+			[key: string]: {
+				[key: string]: any;
+			};
+		} = {
+			products: {
+				details: ProductDetails
+			}
+		};
+
+		return routesMap[data.page][data.sub];
+	});
+</script>
+
+<Pages id={detailId} />
