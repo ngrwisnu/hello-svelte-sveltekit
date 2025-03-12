79d0e118e54df11cb3c14bb9c5415f794b9d89e0
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-03-12 13:08:08 +0700

    update avatar dropdown

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { Menu } from 'lucide-svelte';
+	import { LogOut, Menu } from 'lucide-svelte';
+	import Button from '../ui/Button.svelte';
 
 	type Props = {
 		sidenavHandler: () => void;
@@ -22,7 +23,7 @@
 			<a href="/" class="text-xl font-semibold"> Dashboard </a>
 		</div>
 		<div class="flex-none">
-			<div class="dropdown dropdown-end">
+			<div class="dropdown dropdown-hover dropdown-end">
 				<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
 					<div class="w-10 rounded-full">
 						<img
@@ -31,9 +32,8 @@
 						/>
 					</div>
 				</div>
-				<ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-					<li><a href="/">Settings</a></li>
-					<li><a href="/">Logout</a></li>
+				<ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow">
+					<Button class="justify-start text-red-500"><LogOut size={20} /> Logout</Button>
 				</ul>
 			</div>
 		</div>
