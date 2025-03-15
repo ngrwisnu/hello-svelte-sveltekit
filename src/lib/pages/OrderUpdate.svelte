3aa5d4138729a118354644fcd7c9097394429435
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-03-15 11:06:19 +0700

    update content in update order page

--- src/lib/pages/OrderUpdate.svelte
+++ src/lib/pages/OrderUpdate.svelte
@@ -4,10 +4,10 @@
 	import { CalendarDays } from 'lucide-svelte';
 	import Badge from '$lib/components/ui/Badge.svelte';
 	import Card from '$lib/components/ui/Card.svelte';
-	import CardHeader from '$lib/components/ui/CardHeader.svelte';
 	import CardContent from '$lib/components/ui/CardContent.svelte';
 	import CardFooter from '$lib/components/ui/CardFooter.svelte';
 	import Input from '$lib/components/ui/Input.svelte';
+	import thumb from '$lib/assets/images/default-image.webp';
 
 	const { id } = $props();
 </script>
@@ -30,72 +30,110 @@
 		</div>
 	</div>
 	<div class="mt-8 flex flex-col gap-6 md:flex-row">
-		<Card id="shipping-address" class="h-fit max-w-[500px] min-w-[250px] md:order-2">
-			<CardHeader class="border-b border-slate-200">
-				<h4 class="text-lg font-medium">Customer</h4>
-			</CardHeader>
-			<CardContent>
-				<h6 class="mb-1.5 font-medium">Delivery address</h6>
-				<div class="">John Doe</div>
-				<div class="">+12 123123123</div>
-				<div class="mt-1">123 Main St, Apt 4B</div>
-				<div class="">New York</div>
-				<div class="">NY</div>
-			</CardContent>
-		</Card>
-		<div class="flex-1 space-y-6 md:order-1">
-			<Card id="items-summary" class="w-full">
-				<CardHeader class="flex items-center gap-4 border-b border-slate-200">
-					<h4 class=" text-lg font-medium">Items</h4>
-				</CardHeader>
-			</Card>
-			<Card id="delivery-status" class="w-full">
-				<CardHeader class="flex items-center gap-4 border-b border-slate-200">
-					<h4 class=" text-lg font-medium">Delivery Status</h4>
-					<div>
-						<Badge class="badge-warning"
-							><div aria-label="warning" class="status bg-orange-500"></div>
-							pending</Badge
-						>
-					</div>
-				</CardHeader>
+		<div class="md:order-2">
+			<h4 class="py-2 text-lg font-medium">Customer</h4>
+			<Card id="shipping-address" class="h-fit max-w-[500px] min-w-[250px]">
 				<CardContent>
-					<div class="mb-2 flex w-full items-center justify-between">
-						<div>Tracking number</div>
-						<Input placeholder="Set tracking number" class="max-w-1/2 bg-slate-100" />
-					</div>
+					<h6 class="mb-1.5 font-medium">Delivery address</h6>
+					<div class="">John Doe</div>
+					<div class="">+12 123123123</div>
+					<div class="mt-1">123 Main St, Apt 4B</div>
+					<div class="">New York</div>
+					<div class="">NY</div>
 				</CardContent>
-				<CardFooter class="flex">
-					<Button class="btn-outline ml-auto">Save change</Button>
-				</CardFooter>
 			</Card>
-			<Card id="payment-summary" class="w-full">
-				<CardHeader class="flex items-center gap-4 border-b border-slate-200">
-					<h4 class=" text-lg font-medium">Payment Summary</h4>
-					<div>
-						<Badge class="badge-success"
-							><div aria-label="success" class="status bg-green-500"></div>
-							settlement</Badge
-						>
-					</div>
-				</CardHeader>
-				<CardContent>
-					<div class="mb-2 flex w-full justify-between">
-						<span>Subtotal</span>
-						<span class="font-medium">$9.00</span>
-					</div>
-					<div class="mb-2 flex w-full justify-between">
-						<span>Delivery</span>
-						<span class="font-medium">$0.00</span>
-					</div>
-				</CardContent>
-				<CardFooter class="bg-slate-100">
-					<div class="mb-2 flex w-full justify-between">
-						<span>Paid by Customer</span>
-						<span class="font-medium">$9.00</span>
-					</div>
-				</CardFooter>
-			</Card>
+		</div>
+		<div class="flex-1 space-y-4 md:order-1">
+			<div>
+				<h4 class="py-2 text-lg font-medium">Items</h4>
+				<Card id="items-summary" class="@container w-full">
+					<CardContent class="space-y-2">
+						<div class="grid grid-cols-3 gap-2 @lg:grid-cols-12">
+							<div class="row-span-3 h-auto w-auto max-w-30 min-w-20 @lg:col-span-3">
+								<img src={thumb} alt="product thumbnail" />
+							</div>
+							<div class="font-medium @max-lg:col-start-2 @lg:col-span-3">Black Honey 700gr</div>
+							<div class="@max-lg:col-start-2 @lg:col-span-2">Rp 70.000</div>
+							<div
+								class="text-slate-500 @max-lg:col-start-2 @lg:col-span-2 @lg:justify-self-center"
+							>
+								x1
+							</div>
+							<div
+								class="row-start-1 justify-self-end font-medium @max-lg:col-end-4 @lg:col-span-2 @lg:col-end-13"
+							>
+								Rp 70.000
+							</div>
+						</div>
+						<div class="grid grid-cols-3 gap-2 @lg:grid-cols-12">
+							<div class="row-span-3 h-auto w-auto max-w-30 min-w-20 @lg:col-span-3">
+								<img src={thumb} alt="product thumbnail" />
+							</div>
+							<div class="font-medium @max-lg:col-start-2 @lg:col-span-3">Black Honey 700gr</div>
+							<div class="@max-lg:col-start-2 @lg:col-span-2">Rp 70.000</div>
+							<div
+								class="text-slate-500 @max-lg:col-start-2 @lg:col-span-2 @lg:justify-self-center"
+							>
+								x1
+							</div>
+							<div
+								class="row-start-1 justify-self-end font-medium @max-lg:col-end-4 @lg:col-span-2 @lg:col-end-13"
+							>
+								Rp 70.000
+							</div>
+						</div>
+					</CardContent>
+				</Card>
+			</div>
+			<div>
+				<h4 class="py-2 text-lg font-medium">Delivery Status</h4>
+				<Card id="delivery-status" class="w-full">
+					<CardContent>
+						<div class="mb-2 flex w-full justify-between">
+							<div>Status</div>
+							<Badge class="badge-warning"
+								><div aria-label="warning" class="status bg-orange-500"></div>
+								pending</Badge
+							>
+						</div>
+						<div class="mb-2 flex w-full items-center justify-between">
+							<div>Tracking number</div>
+							<Input placeholder="Set tracking number" class="max-w-1/2 bg-slate-100" />
+						</div>
+					</CardContent>
+					<CardFooter class="flex border-t border-slate-200">
+						<Button class="btn-outline ml-auto">Save change</Button>
+					</CardFooter>
+				</Card>
+			</div>
+			<div>
+				<h4 class="py-2 text-lg font-medium">Payment Summary</h4>
+				<Card id="payment-summary" class="w-full">
+					<CardContent>
+						<div class="mb-2 flex w-full justify-between">
+							<div>Status</div>
+							<Badge class="badge-success"
+								><div aria-label="success" class="status bg-green-500"></div>
+								settlement</Badge
+							>
+						</div>
+						<div class="mb-2 flex w-full justify-between">
+							<div>Subtotal</div>
+							<div class="font-medium">Rp 140.000</div>
+						</div>
+						<div class="mb-2 flex w-full justify-between">
+							<div>Delivery</div>
+							<div class="font-medium">Rp 0</div>
+						</div>
+					</CardContent>
+					<CardFooter class="bg-slate-100">
+						<div class="mb-2 flex w-full justify-between">
+							<div>Paid by Customer</div>
+							<div class="font-medium">Rp 140.000</div>
+						</div>
+					</CardFooter>
+				</Card>
+			</div>
 		</div>
 	</div>
 </WrapperMain>
